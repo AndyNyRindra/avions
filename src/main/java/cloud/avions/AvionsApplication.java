582f0079e0f7799aa278499d6446b26c0c77540c
AndyNyRindra <92746662+AndyNyRindra@users.noreply.github.com>
2022-12-20 17:14:03 +0300

    fix cors error

--- src/main/java/cloud/avions/AvionsApplication.java
+++ src/main/java/cloud/avions/AvionsApplication.java
@@ -1,7 +1,11 @@
 package cloud.avions;
 
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AvionsApplication {
@@ -10,4 +14,14 @@ public class AvionsApplication {
         SpringApplication.run(AvionsApplication.class, args);
     }
 
+    @Bean
+    WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/*").allowedOriginPatterns("*")
+                        .allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+            }
+        };
+    }
 }
